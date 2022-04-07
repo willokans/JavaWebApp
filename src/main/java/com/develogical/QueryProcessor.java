8552653dc34bdadbbca022dad5ad030186e40018
willokans <Slide2014!>
2022-04-07 14:55:58 +0100

    what is your name test2

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -7,7 +7,9 @@ public class QueryProcessor {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
+        } else if (query.toLowerCase().contains("what is your name")) {
+            return "Something!";
         }
-        return "Something!";
+        return "";
     }
 }
